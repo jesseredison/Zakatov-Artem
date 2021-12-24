67930bac70596e68dd89dd7bbe9c5dc9c41e28b4
jesseredison <96612303+jesseredison@users.noreply.github.com>
2021-12-24 11:10:59 +0300

    Create 8.jl

---
+++ 8.jl
@@ -0,0 +1,33 @@
+function main(r)
+    counter = 1
+    flag = false
+    while(isborder(r, Nord) & !flag)
+        c = counter
+        while ((c>0) & !flag)
+            move!(r, West)
+            if(!isborder(r, Nord))
+                flag = true
+            end
+            c = c - 1
+        end
+        c = counter
+        while((c>0) & !flag)
+            move!(r, Ost)
+            c = c - 1
+        end
+        c = counter
+        while ((c>0) & !flag)
+            move!(r, Ost)
+            if(!isborder(r, Nord))
+                flag = true
+            end
+            c = c - 1
+        end
+        while((c>0) & !flag)
+            move!(r, West)
+            c = c - 1
+        end
+        c = counter
+        counter = counter + 1
+    end
+end
